2dcb1119533e77b00e7a5a720fa1042e6c8a8752
diegomeyer <diego_r_meyer@hotmail.com>
2025-03-18 20:21:12 -0300

    Corrigindo start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -55,4 +55,3 @@ docker exec -it hadoop-datanode bash -c "unset HADOOP_OPTS && hdfs dfs -mkdir /d
 docker exec -it hadoop-datanode bash -c "unset HADOOP_OPTS && hdfs dfs -chmod -R 777 /datalake"
 
 echo "Todos os serviços foram iniciados e o tópico foi criado com sucesso."
-unset HADOOP_OPTS && hdfs dfs -ls /
